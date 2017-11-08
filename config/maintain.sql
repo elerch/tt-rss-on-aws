5b72c7cd873174ba5dfb2aa5457dcfcf95c73e8a
Emil Lerch <emil@lerch.org>
2017-11-08 15:02:30 -0800

    also remove all counters to prevent ghost read counts

--- config/maintain.sql
+++ config/maintain.sql
@@ -1,3 +1,6 @@
 DELETE FROM ttrss_user_entries USING ttrss_entries, ttrss_user_entries WHERE id = ref_id AND date_entered < DATE_SUB(NOW(), INTERVAL 14 DAY) AND marked = FALSE and unread =0;
 
 DELETE FROM ttrss_entries WHERE (SELECT COUNT(int_id) FROM ttrss_user_entries WHERE ref_id = id) = 0;
+
+DELETE FROM ttrss_cat_counters_cache;
+DELETE FROM ttrss_counters_cache;
